a46626551cf10f15a1f51d0c9a3cb4671ecb1bb1
Michael Harris <michaeljh7@gmail.com>
2018-05-02 17:26:25 -0600

    Added Comment resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502225305) do
+ActiveRecord::Schema.define(version: 20180502231034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20180502225305) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.integer "user_id"
     t.string "user_type"
@@ -31,4 +41,6 @@ ActiveRecord::Schema.define(version: 20180502225305) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
   )
 end
 puts "10 news articles created!"
+
+
+User.create!(user_id: 1, user_type: 'ADMIN', user_name: "Michael")
+puts "1 admin user created!"
+
+User.create!(user_id: 1, user_type: 'USER', user_name: "Mike")
+puts "1 standard user created!"
